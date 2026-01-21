da69ef20c1bf1c82f36566f05b4f63123c813417
mr-lemoncello <vikasarino@gmail.com>
2026-01-20 16:01:36 -0800

    idk 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    customElement: true
-  },
-  kit: {
-    adapter: adapter({
-      out: '.',
-      fallback: '404.html'
-    }),
-    paths: {
-      base: '',
-    },
-  },
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
